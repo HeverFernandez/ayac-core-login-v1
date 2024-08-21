47390257bf25c5c020c49f50ce42bceb4851bbfe
aitamh.fg@gmail.com <46291409hfg>
2024-08-20 22:33:20 -0500

    first commit

---
+++ src/main/java/com/ayac_core_login/ayac_core_login/AyacCoreLoginApplication.java
@@ -0,0 +1,13 @@
+package com.ayac_core_login.ayac_core_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyacCoreLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyacCoreLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ayac_core_login/ayac_core_login/AyacCoreLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayac_core_login.ayac_core_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyacCoreLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
